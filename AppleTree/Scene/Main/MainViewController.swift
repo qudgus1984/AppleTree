3821861084709ac10fec23fe9ad0eb3a016ac06c
ByeongHyeon <qudgus1984@naver.com>
2022-10-12 16:29:22 +0900

    notification 알림 화면전환 extension 구현

--- AppleTree/Scene/Main/MainViewController.swift
+++ AppleTree/Scene/Main/MainViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import RealmSwift
 import Toast
+import FirebaseAnalytics
 
 final class MainViewController: BaseViewController {
     
@@ -350,3 +351,30 @@ extension MainViewController: settingTimeDelegate {
         mainview.settingCount = getSettingTime.startIndex
     }
 }
+
+extension UIViewController {
+    
+    var topViewController: UIViewController? {
+        return self.topViewController(currentViewController: self)
+    }
+    
+    //최상위 뷰컨트롤러를 판단해주는 메서드
+    func topViewController(currentViewController: UIViewController) -> UIViewController {
+        if let tabBarController = currentViewController as? UITabBarController, let selectedViewController = tabBarController.selectedViewController {
+            
+            return self.topViewController(currentViewController: selectedViewController)
+            
+        } else if let navigationController = currentViewController as? UINavigationController, let visibleViewController = navigationController.visibleViewController {
+            
+            return self.topViewController(currentViewController: visibleViewController)
+            
+        } else if let presentedViewController = currentViewController.presentedViewController {
+            
+            return self.topViewController(currentViewController: presentedViewController)
+            
+        } else {
+            return currentViewController
+        }
+    }
+}
+
